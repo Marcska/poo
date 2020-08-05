77f0bb5391dda4b3eb8eeb356cfa722a34d4b82d
Marcska <marcska97@gmail.com>
2020-08-05 18:18:11 -0500

    Se agrrega actividad del 5 de agosto y del 3 de agosto

---
+++ primer_git/sesion_3_agosto/src/ico/fes/poo/Teclado.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo;
+
+/**
+ *
+ * @author marcska
+ */
+public class Teclado {
+    int numeroTeclas;
+    public void teclear(int keyCode){
+        System.out.println("Se presionó la tecla :" + keyCode);
+    }
+}
